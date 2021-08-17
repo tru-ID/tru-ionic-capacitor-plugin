4d8ca53ec0dd792c6d3d385ff4ec8cfc50e5b9f7
glamboyosa <ogbemudiatimothy@gmail.com>
2021-08-17 13:57:02 +0100

    feat: iOS implementation

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-# tru-plugin-ionic-capacitor
+# tru.ID Capacitor / Ionic Plugin 
 
-tru.ID plugin that exposes methods for working with the tru.ID product API
 
 ## Install
 
@@ -9,11 +8,4 @@ npm install tru-plugin-ionic-capacitor
 npx cap sync
 ```
 
-## API
 
-<docgen-index></docgen-index>
-
-<docgen-api>
-<!-- run docgen to generate docs from the source -->
-<!-- More info: https://github.com/ionic-team/capacitor-docgen -->
-</docgen-api>
--- ios/Plugin/TruPluginIonicCapacitorPlugin.swift
+++ ios/Plugin/TruPluginIonicCapacitorPlugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import tru_sdk_ios
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -9,10 +10,80 @@ import Capacitor
 public class TruPluginIonicCapacitorPlugin: CAPPlugin {
     private let implementation = TruPluginIonicCapacitor()
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
+   
+    @objc func check(_ call: CAPPluginCall) {
+       guard let url = call.getString("url") as? String else {
+           call.reject("Must provide a URL")
+           return
+       }
+
+         guard let nurl = URL(string: url) else {
+           call.reject("Unable to create a url.")
+           return
+       }
+
+        let truSdk: TruSDK = TruSDK()
+        truSdk.check(url: nurl) { error in
+            if let error = error as NSError? {
+               call.reject(error.localizedDescription, nil, error)
+            } else {
+                call.resolve([
+            "result": url
         ])
+            }
+        }
+       
+    }
+
+      
+       @objc func checkWithTrace(_ call: CAPPluginCall) {
+       guard let url = call.getString("url") as? String else {
+           call.reject("Must provide a URL")
+           return
+       }
+
+         guard let nurl = URL(string: url) else {
+           call.reject("Unable to create a url.")
+           return
+       }
+
+        let truSdk: TruSDK = TruSDK()
+        truSdk.checkWithTrace(url: nurl) { error, traceInfo in
+            if let error = error {
+                call.reject("Error from checkWithTrace =>\(error)", nil, error)
+                return
+            }
+
+            guard let trace = traceInfo else {
+                call.reject("There is not error received, however TraceInfo is not available")
+                return
+            }
+
+            call.resolve([
+                result: trace.trace
+            ])
+        }
+       
+    }
+
+    @objc func checkWithTrace(_ call: CAPPluginCall) {
+
+      let truSdk: TruSDK = TruSDK()
+      truSdk.isReachable{ result in
+        switch(result) {
+          case .success(let reachabilityDetails):
+               do{
+                    let jsonData = try JSONEncoder().encode(reachabilityDetails)
+                    let jsonString = String(data: jsonData, encoding: .utf8)!
+                    call.resolve([
+                        "result": jsonString
+                    ])
+                } catch {
+                    call.reject("Unable to decode reachability details to json")
+                }
+            case .failure(let error):
+                call.reject(error.localizedDescription, nill, error)
+            }
+        }
     }
 }
